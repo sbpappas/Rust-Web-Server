954fa91de3a185e8725982e61b9f7fecace530d8
Samuel <Samuel@Samuels-MacBook-2.local>
2024-02-14 16:04:41 -0600

    updating threading

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,11 @@ impl ThreadPool {
         assert!(size > 0);
 
         let mut threads = Vec::with_capacity(size);
-    
+
         for _ in 0..size {
             // create some threads and store them in the vector
         }
-    
+
         ThreadPool { threads }
     }
     pub fn execute<F>(&self, f: F)
