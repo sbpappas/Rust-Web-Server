96121a2048deb513e1e17baf6ba0a4dc07e234cb
Samuel <Samuel@Samuels-MacBook-2.local>
2024-02-14 17:19:46 -0600

    we have a working multi threaded web server in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Mutex;
+use std::sync::Arc;
+
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -14,21 +21,46 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
 
     //pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
-
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let mut threads = Vec::with_capacity(size);
-
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        pub fn new(size: usize) -> ThreadPool {
+            assert!(size > 0);
+    
+            let (sender, receiver) = mpsc::channel();
+    
+            let receiver = Arc::new(Mutex::new(receiver));
+    
+            let mut workers = Vec::with_capacity(size);
+    
+            for id in 0..size {
+                workers.push(Worker::new(id, Arc::clone(&receiver)));
+            }
+    
+            ThreadPool { workers, sender }
         }
+        pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+        {
+            let job = Box::new(f);
+    
+            self.sender.send(job).unwrap();
+        }
+}
 
-        ThreadPool { threads }
-    }
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker { id, thread }
     }
 }
